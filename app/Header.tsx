e7e4abd25f33f43cfcc595b973ff6e8183419214
levoanhhao <levoanhhao.engineer@gmail.com>
2023-03-03 00:20:43 +0700

    cap nhat

---
+++ app/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='bg-blue-300 text-white text-2xl text-center'><p>i am header</p></header>
+  )
+}
+
+export default Header
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+
+
+const Home = () => {
+  return (
+    <div className='text-red-500'>i am the home page</div>
+  )
+}
+
+export default Home
